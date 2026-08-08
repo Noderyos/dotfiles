9f029f34939a924c1f01bdfbc3c00f28adf2b133
Noderyos <vincent.bsod@gmail.com>
2026-08-08 17:47:22 +0200

    Switching to NVIM 0.12 plugin system
    Half is proken LOL
    Dont use.
    please.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,17 +1,3 @@
--- insures lazy is installed
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
 -- include plugins and their config
 require('plugins')
 require('plug_config.treesitter')
--- nvim/lua/plug_config/pio.lua
+++ nvim/lua/plug_config/pio.lua
@@ -1,5 +1,5 @@
 require('platformio').setup({
-  lsp = 'clangd', --default: ccls, other option: clangd
+  lsp = 'ccls', --default: ccls, other option: clangd
   -- If you pick clangd, it also creates compile_commands.json
   -- Comment out following line if you want to disable the piomenu.
   menu_key = '<leader>p',
